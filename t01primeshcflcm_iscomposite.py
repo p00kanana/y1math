4cf6e29b88a721ee556b1dca3fdb339d1f9ac561
p00kanana <52941304+p00kanana@users.noreply.github.com>
2019-10-17 14:25:05 +0800

    Update t01primeshcflcm_iscomposite.py

--- t01primeshcflcm_iscomposite.py
+++ t01primeshcflcm_iscomposite.py
@@ -1,6 +1,7 @@
 # Is num a composite number? 
 # A composite number is an integer with more than 2 different factors / is a product of at least 2 primes.
 
+
 num = 63
 num_factors = 0
 for i in range(1,num+1):
